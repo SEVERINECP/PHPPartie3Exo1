a82d53455bf427e9a0f4d883a73e789fa72fb726
SEVERINECP <sevpicot@gmail.com>
2019-12-04 16:33:38 +0100

    PHPPartie3Exo1-2-3End

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8" />
+    <title>PHPPartie3Exo1Exo2Exo3</title>
+  </head>
+  <body>
+    <?php
+      $months = array ('Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Aout', 'Septembre', 'Octobre', 'Novembre', 'Décembre');
+      echo $months[5];
+      foreach($months as $elements)
+      {
+      echo $elements;
+      }
+    ?>
+  </body>
+</html>
