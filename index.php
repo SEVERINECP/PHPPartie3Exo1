76c3683298341a1f6790ec2c064e2261eff6b744
SEVERINECP <sevpicot@gmail.com>
2019-12-05 09:34:08 +0100

    PHPPartie3Exo1-2-3Corrigés

--- index.php
+++ index.php
@@ -7,10 +7,16 @@
   <body>
     <?php
       $months = array ('Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Aout', 'Septembre', 'Octobre', 'Novembre', 'Décembre');
+      /*autre possibilité de code:
+      $months = array;
+      $months = [0];
+      $months = [1]; ect...*/
       echo $months[5];
+      //Pour chauqe éléments du tableau mois, on affiche ses éléments
       foreach($months as $elements)
       {
       ?>
+      <!--Pour positionner les éléments les uns en dessous des autres:-->
       <p><?php echo $elements; ?></p>
     <?php } ?>
   </body>
